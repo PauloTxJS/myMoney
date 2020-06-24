a7bb3212afa83f859a48871934542bae1388d4af
Paulo Teixeira <paulo.teixeira541@gmail.com>
2020-06-23 21:15:09 -0300

    Acrescentei a função inserir dados e deletar dados

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import useGet from './useGet';
+import usePost from './usePost';
+import useDelete from './useDelete';
+import axios from 'axios';
 
 const url = 'https://mymoney-pauloteixeira.firebaseio.com/movimentacoes/2020-06.json'
 
 function App() {
   const data = useGet(url)
-  const data2 = useGet('http://httpbin.org/ip')
+  const [postData, post] = usePost(url)
+  const [deleteData, remove] = useDelete()
+  
+  const saveNew = () => {
+    post({ valor: 10, descricao: 'olá' })
+  }
+
+  const doRemove = () => {
+    remove('https://mymoney-pauloteixeira.firebaseio.com/movimentacoes/2020-06/-MAXMhKoQ0yK0EbXEsao.json')
+  }
+
   return (
     <div>
       <h1>MyMoney</h1>
       { JSON.stringify(data) }
       { data.loading && <p>Loading...</p> }
-      <pre>{ JSON.stringify(data2) }</pre>
+      <button onClick={saveNew}>Salvar</button>
+      <pre>{JSON.stringify(postData)}</pre>
+      <button onClick={doRemove}>Deletar</button>
+      <pre>{JSON.stringify(deleteData)}</pre>
     </div>
   );
 }
