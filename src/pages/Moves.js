b576e9061f856f2cfd2af480e7853febad07558a
Paulo Teixeira <paulo.teixeira541@gmail.com>
2020-06-25 11:33:48 -0300

    Adicionando links

--- src/pages/Moves.js
+++ src/pages/Moves.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import Rest from '../usefull/rest';
 
-const Moves = () => {
-    return <h1>Movimentações</h1>
+const baseURL = 'https://mymoney-pauloteixeira.firebaseio.com/'
+const { useGet } = Rest(baseURL)
+
+const Moves = ({ match }) => {
+    const data = useGet(`movimentacoes/${match.params.data}`)
+    return (
+        <div className='container'>
+            <h1>Movimentações</h1>
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>Descrição</th>
+                        <th>Valor</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { data.data &&   
+                        Object
+                            .keys(data.data)
+                            .map(moves => {
+                                return (
+                                    <tr>
+                                        <td>{data.data[moves].descricao}</td>
+                                        <td>{data.data[moves].valor}</td>
+                                    </tr>
+                                )
+                            })
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default Moves;
