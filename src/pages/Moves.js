f7ad4195701074a1f0b94ba505ba074b7748c7cb
Paulo Teixeira <paulo.teixeira541@gmail.com>
2020-06-25 18:05:58 -0300

    Terminei o incluir e apagar

--- src/pages/Moves.js
+++ src/pages/Moves.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import { useState } from 'react';
 import Rest from '../usefull/rest';
 
 const baseURL = 'https://mymoney-pauloteixeira.firebaseio.com/'
-const { useGet } = Rest(baseURL)
+const { useGet, usePost, useDelete } = Rest(baseURL)
 
 const Moves = ({ match }) => {
     const data = useGet(`movimentacoes/${match.params.data}`)
+    const [postData, save] = usePost(`movimentacoes/${match.params.data}`)
+    const [removeData, remove] = useDelete()
+    const [description, setDescription] = useState('')
+    const [value, setValue] = useState(0.0)
+    
+    const onChangeDescription = evt => {
+        setDescription(evt.target.value);
+    } 
+
+    const onChangeValue = evt => {
+        setValue( parseFloat(evt.target.value) );
+    }
+    
+    const saveMoves = async() => {
+        await save({
+            descricao: description,
+            valor: value  
+        })
+        setDescription('')
+        setValue(0)
+        data.refetch()
+    }
+
+    const removeMoves = async(id) => {
+        await remove(`movimentacoes/${match.params.data}/${id}`)
+        data.refetch()
+    }
+    
     return (
         <div className='container'>
             <h1>Movimentações</h1>
@@ -23,12 +52,26 @@ const Moves = ({ match }) => {
                             .map(moves => {
                                 return (
                                     <tr>
-                                        <td>{data.data[moves].descricao}</td>
-                                        <td>{data.data[moves].valor}</td>
+                                        <td>
+                                            {data.data[moves].descricao}
+                                        </td>
+                                        <td>
+                                            {data.data[moves].valor}
+                                            <button onClick={() => removeMoves(moves)}>-</button>
+                                        </td>
                                     </tr>
                                 )
                             })
                     }
+                    <tr>
+                        <td>
+                            <input type='text' value={description} onChange={onChangeDescription}/>
+                        </td>
+                        <td>
+                            <input type='text' value={value} onChange={onChangeValue}/>
+                            <button onClick={saveMoves}>+</button>
+                        </td>
+                    </tr>
                 </tbody>
             </table>
         </div>
