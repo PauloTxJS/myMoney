9f1fc9ee7be535dc83dc13105e33a0899c971e7f
Paulo Teixeira <paulo.teixeira541@gmail.com>
2020-06-29 09:48:24 -0300

    Fiz o redirecionamento

--- src/pages/Home/AddMonth.js
+++ src/pages/Home/AddMonth.js
@@ -1,18 +1,49 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+import { Redirect } from 'react-router-dom'
+
+const minYear = 2020
+const maxYear = 2022
 
 const AddMonth = () => {
+
+    const refYear = useRef()
+    const refMonth = useRef()
+    const [redir, setRedir] = useState('') 
+    const years = []
+    const months = []
+
+
+    for(let i = minYear; i <= maxYear; i++) {
+        years.push(i);        
+    }
+    for(let i = 1; i <= 12; i++) {
+        months.push(i);        
+    }
+    
+    const zeroPad = num => {
+        if(num < 10) {
+            return '0' + num; 
+        }
+        return num;
+    }
+
+    const lookMonth = () => {
+        setRedir(refYear.current.value + '-' + refMonth.current.value);
+    }
+    if(redir !== '') {
+        return <Redirect to={'/movimentacoes/' + redir} />
+    }
+
     return (
         <React.Fragment>
             <h2>Adicionar mês</h2>
-            <select>
-            <option value='2020'>2020</option>
-            <option value='2021'>2021</option>
+            <select ref={refYear}>
+                {years.map(year => <option key={year} value={year}>{year}</option>)}
             </select>
-            <select>
-            <option value='01'>01</option>
-            <option value='02'>02</option>
+            <select ref={refMonth}>
+                {months.map(zeroPad).map(month => <option key={month} value={month}>{month}</option>)}
             </select>
-            <button>Adicionar mês</button>
+            <button onClick={lookMonth}>Adicionar mês</button>
         </React.Fragment>
     )
 }
